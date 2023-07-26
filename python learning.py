1c54f6b74978c2c07a0330cf07f2fdb35feed211
Nellythepelly <Neilsehgal2021@gmail.com>
2023-07-26 12:39:31 -0400

    Update python learning.py

--- python learning.py
+++ python learning.py
@@ -541,3 +541,22 @@ for i in l:
     # your code here
     my_lambda = lambda x : (x % 2) == 1
     print(my_lambda(i))
+
+
+import json
+
+# fix this function, so it adds the given name
+# and salary pair to salaries_json, and return it
+def add_employee(salaries_json, name, salary):
+    salaries = json.loads(salaries_json)
+    salaries[name] = salary
+
+    return json.dumps(salaries)
+
+# test code
+salaries = '{"Alfred" : 300, "Jane" : 400 }'
+new_salaries = add_employee(salaries, "Me", 800)
+decoded_salaries = json.loads(new_salaries)
+print(decoded_salaries["Alfred"])
+print(decoded_salaries["Jane"])
+print(decoded_salaries["Me"])
