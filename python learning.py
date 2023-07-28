588d62f480a5590ea663d05c31959db50e6431e1
Nellythepelly <Neilsehgal2021@gmail.com>
2023-07-28 13:36:52 -0400

    Update python learning.py

--- python learning.py
+++ python learning.py
@@ -574,3 +574,23 @@ class Vehicle:
 
 # Print a list of all attributes of the Vehicle class.
 print(dir(Vehicle))
+
+
+
+import json
+
+# fix this function, so it adds the given name
+# and salary pair to salaries_json, and return it
+def add_employee(salaries_json, name, salary):
+    salaries = json.loads(salaries_json)
+    salaries[name] = salary
+
+    return json.dumps(salaries)
+
+# test code
+salaries = '{"Alfred" : 300, "Jane" : 400 }'
+new_salaries = add_employee(salaries, "Me", 800)
+decoded_salaries = json.loads(new_salaries)
+print(decoded_salaries["Alfred"])
+print(decoded_salaries["Jane"])
+print(decoded_salaries["Me"])
