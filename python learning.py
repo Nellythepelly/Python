950aada5c4edd16142221410b1bf89859e3bc55d
Nellythepelly <Neilsehgal2021@gmail.com>
2023-08-02 10:23:05 -0400

    Update python learning.py

--- python learning.py
+++ python learning.py
@@ -628,3 +628,11 @@ def first_letter(word):
 
 print(first_letter('Hello World'))
 first_letter(['Not', 'A', 'String'])
+
+
+from functools import partial
+def func(u, v, w, x):
+    return u*4 + v*3 + w*2 + x
+
+p = partial(func,5,6,7)
+print(p(8))
