4aa15d94630d155fffb9eadfa2962cae65bd5fe9
Nellythepelly <Neilsehgal2021@gmail.com>
2023-08-21 10:25:27 -0400

    Create quiz game.py

---
+++ quiz game.py
@@ -0,0 +1,27 @@
+# --------------------
+def new_game():
+    pass
+
+
+# -----------------------
+def check_answer():
+    pass
+
+
+# -------------------
+def display_score():
+    pass
+
+
+# --------------------
+def play_again():
+    pass
+# --------------------
+
+
+questions = {
+    "Who created Python?: ": "A",
+    "What was the year Python was created?: ": "B",
+    "Pyhton is tributed to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"
+}
